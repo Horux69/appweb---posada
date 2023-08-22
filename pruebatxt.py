e2ee4d9b3e8511cb71cd256a14f3b66a2920a1ee
unknown <tu_correo@example.com>
2023-08-22 12:02:57 -0500

    0.6

--- pruebatxt.py
+++ pruebatxt.py
@@ -4,7 +4,11 @@ cod_p1 = string[0:3]
 string = "ORIENTE"
 cod_p2 = string[0:3]
 
-cod_p3 = "0001"
+cod_p3 = 0
+
+cod_p3 += 1
+cod_p3 = str(cod_p3).zfill(4)
+
 
 cod_sitio = cod_p1 + cod_p2 + cod_p3
 
